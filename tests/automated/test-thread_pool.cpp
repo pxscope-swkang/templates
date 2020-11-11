751731774114d17fd05425711c4e6524343bb204
Stephen Kang <ki6080@gmail.com>
2020-11-11 11:54:29 +0900

    Improve test for timer_thread_pool

--- tests/automated/test-thread_pool.cpp
+++ tests/automated/test-thread_pool.cpp
@@ -8,9 +8,9 @@
 using namespace kangsw;
 using namespace std;
 namespace kangsw::thread_pool_test {
-constexpr int num_cases = 256;
 TEST_CASE("[timer_thread_pool] thread pool default operation")
 {
+    constexpr int NUM_CASE = 1024;
     printf("<< THREAD POOL TEST >>");
     for (int ITER = 1; ITER; ITER--) {
         printf("\n [%4d] -------------------------------- \n", ITER);
@@ -20,37 +20,37 @@ TEST_CASE("[timer_thread_pool] thread pool default operation")
         thr.max_task_wait_time = 100ms;
         thr.max_stall_interval_time = 1022322ms;
         thr.average_weight = 10;
-        static array<pair<double, std::shared_ptr<future_proxy<double>>>, num_cases> futures;
-        static array<char, num_cases> executed_list;
+        static array<pair<double, std::shared_ptr<future_proxy<double>>>, NUM_CASE> futures;
+        static array<char, NUM_CASE> executed_list;
         memset(executed_list.data(), 0, sizeof executed_list);
 
         atomic_int fill_index = 0;
-        static array<chrono::system_clock::time_point, num_cases> finish_time;
+        static array<chrono::system_clock::time_point, NUM_CASE> finish_time;
         auto pivot_time = chrono::system_clock::now();
 
         thr.num_max_workers(256);
 
         // multithreaded enqueing
-        counter_range counter(num_cases);
+        counter_range counter(NUM_CASE);
         for_each(std::execution::par_unseq, counter.begin(), counter.end(), [&](size_t i) {
             auto exec_time = chrono::system_clock::now() + chrono::milliseconds(rand() % 1200);
             futures[i] = make_pair(
               (double)i,
               thr.add_timer(
                    exec_time, [&, at_exec = exec_time](double c) {
-                       this_thread::sleep_for(chrono::milliseconds(rand() % 8));
+                       this_thread::sleep_for(chrono::milliseconds(rand() % 43));
                        finish_time[fill_index++] = chrono::system_clock::time_point(at_exec - pivot_time);
                        executed_list[static_cast<size_t>(c + 0.5)] = 1;
                        return (int)(c + 0.5);
                    },
                    (double)i)
                 ->then([&](int c) {
-                    this_thread::sleep_for(chrono::milliseconds(rand() % 25));
+                    this_thread::sleep_for(chrono::milliseconds(rand() % 14));
                     executed_list[static_cast<size_t>(c + 0.5)] = 2;
                     return (double)c;
                 })
                 ->then([i]() {
-                    this_thread::sleep_for(chrono::milliseconds(rand() % 16));
+                    // this_thread::sleep_for(chrono::milliseconds(rand() % 16));
                     executed_list[static_cast<size_t>(i + 0.5)] = 3;
                     return (double)i * i;
                 }));
@@ -89,7 +89,7 @@ TEST_CASE("[timer_thread_pool] thread pool default operation")
         }
         display += "}";
         INFO(display);
-        CHECK(std::count(executed_list.begin(), executed_list.end(), 3) == num_cases);
+        CHECK(std::count(executed_list.begin(), executed_list.end(), 3) == NUM_CASE);
         CHECK(finish_time.back() > finish_time.front());
 
         for (auto& pair : futures) {
@@ -109,24 +109,35 @@ TEST_CASE("[timer_thread_pool] Timer accuracy test")
 {
     using namespace std::chrono;
 
-    static constexpr int NUM_CASE = 38464;
+    static constexpr int NUM_CASE = 5120;
     timer_thread_pool workers;
 
-    vector<system_clock::duration> errors = {};
-    errors.resize(NUM_CASE);
+    vector<system_clock::duration> errors{NUM_CASE};
 
     std::for_each(
       std::execution::par_unseq, errors.begin(), errors.end(),
       [&](system_clock::duration& error_dest) {
-          auto delay = 200us * (rand() % 1000);
+          auto delay = 500us * (rand() % 1000) + (1us * rand() % 500) + 10ms;
           auto issue = system_clock::now();
 
+          /*
+          std::thread([&error_dest, delay, issue]() {
+              std::this_thread::sleep_until(issue + delay);
+              error_dest = (system_clock::now() - issue) - delay;
+          }).detach();
+          /*/
           workers.add_timer(delay, [&error_dest, delay, issue]() {
               error_dest = (system_clock::now() - issue) - delay;
           });
+          //*/
+          // std::this_thread::sleep_for(1us);
       });
 
-    std::this_thread::sleep_for(500ms);
+    while (workers.num_total_waitings() != 0 && workers.num_available_workers() != 0) {
+        std::this_thread::sleep_for(200ms);
+    }
+    std::this_thread::sleep_for(1000ms);
+
     std::sort(errors.begin(), errors.end());
     auto percent_5 = NUM_CASE * 5 / 100;
     auto avg_err = std::reduce(errors.begin(), errors.end()) / NUM_CASE;
@@ -134,15 +145,11 @@ TEST_CASE("[timer_thread_pool] Timer accuracy test")
     auto max_v = std::reduce(errors.end() - percent_5, errors.end()) / percent_5;
 
     auto to_micro = [](auto value) { return duration<double, std::micro>(value).count(); };
-    auto min_mult_by_N = (to_micro(min_v) * NUM_CASE);
-    auto max_div_by_N = to_micro(max_v) / NUM_CASE;
-    INFO("Num Case        : " << NUM_CASE);
-    INFO("Average Error Is: " << to_micro(avg_err) << " us");
-    INFO("Min Error       : " << to_micro(min_v) << " us");
-    INFO("Max Div by N    : " << max_div_by_N << " us");
-    INFO("Average Wait    : " << to_micro(workers.average_wait()) << " us");
-    INFO("Max Error       : " << to_micro(max_v) << " us");
-    INFO("Min Mult by N   : " << min_mult_by_N << " us");
+    INFO("Num Case         : " << NUM_CASE);
+    INFO("Average Error Is : " << to_micro(avg_err) << " us");
+    INFO("Min Error        : " << to_micro(min_v) << " us");
+    INFO("Average Wait     : " << to_micro(workers.average_wait()) << " us");
+    INFO("Max Error        : " << to_micro(max_v) << " us");
     CAPTURE(percent_5);
     CAPTURE(workers.num_workers());
     REQUIRE(to_micro(min_v) < 500);

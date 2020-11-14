a2aa1f70041a5e365798b3f7d2473150d538ea93
Stephen Kang <ki6080@gmail.com>
2020-11-14 11:04:55 +0900

    Implement safe string table

--- include/kangsw/hash_index.hxx
+++ include/kangsw/hash_index.hxx
@@ -7,6 +7,7 @@
  * ----------------------------------------------------------------------------
  */
 #pragma once
+#include <shared_mutex>
 #include <string_view>
 #include "misc.hxx"
 
@@ -14,13 +15,11 @@ namespace kangsw {
 struct hash_index {
 public:
     constexpr hash_index(std::string_view str)
-        : hash(impl__::fnv1a_impl(str.data(), str.data() + str.size()))
-    {}
+        : hash(impl__::fnv1a_impl(str.data(), str.data() + str.size())) {}
 
     template <size_t N>
     constexpr hash_index(char const (&str)[N])
-        : hash(fnv1a(str))
-    {}
+        : hash(fnv1a(str)) {}
 
 public:
     constexpr operator size_t() const { return hash; }
@@ -30,6 +29,40 @@ public:
 
 public:
     size_t const hash;
+};
+
+/**
+ * hash_index로부터 이름을 빌드하기 위한 함수성
+ */
+class safe_string_table {
+    std::string_view operator[](hash_index hash) const {
+        std::shared_lock _0(lock_);
+        std::string_view retval = {};
+        if (auto found_it = table_.find(hash); found_it != table_.end()) {
+            retval = found_it->second;
+        }
+        return retval;
+    }
+
+    void push(hash_index hash, std::string val) {
+        // optimistically assumes there is already same hash exists
+        {
+            std::shared_lock _0(lock_);
+            auto found_it = table_.find(hash);
+            ;
+            if (found_it != table_.end()) {
+                return;
+            }
+        }
+
+        // if there is no existing entity, takes lock and emplace given hash.
+        std::unique_lock _1(lock_);
+        table_.try_emplace(hash, std::move(val));
+    }
+
+private:
+    std::unordered_map<hash_index, std::string const> table_;
+    std::shared_mutex lock_;
 };
 
 } // namespace kangsw
